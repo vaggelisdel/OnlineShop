94626fc2c30f390696d49e856bd86cce497cfb37
vaggelisdel <delliosvaggelis@gmail.com>
2019-11-12 15:06:18 +0200

    express-validator

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var mongoose = require('mongoose');
 var session = require('express-session');
 var passport = require('passport');
 var flash = require('connect-flash');
+var validator = require('express-validator');
 
 var routes = require('./routes/index');
 var userRoutes = require('./routes/user');
@@ -28,6 +29,7 @@ app.set('view engine', '.hbs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(validator());
 app.use(cookieParser());
 app.use(session({secret: 'mysupersecret', resave: false, saveUninitialized: false}));
 app.use(flash());
--- routes/user.js
+++ routes/user.js
@@ -2,57 +2,58 @@ var express = require('express');
 var router = express.Router();
 var csrf = require('csurf');
 var passport = require('passport');
+const {check, validationResult} = require('express-validator');
 
 var csrfProtection = csrf();
 router.use(csrfProtection);
 
 router.get('/profile', requiredLogin, function (req, res, next) {
-  res.render('user/profile');
+    res.render('user/profile');
 });
 
 router.get('/logout', requiredLogin, function (req, res, next) {
-  req.logout();
-  res.redirect('/');
+    req.logout();
+    res.redirect('/');
 });
 
-router.use('/', notRequiredLogin, function(req, res, next) {
-  next();
+router.use('/', notRequiredLogin, function (req, res, next) {
+    next();
 });
 
 router.get('/signup', function (req, res, next) {
-  var messages = req.flash('error');
-  res.render('user/signup', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
+    var messages = req.flash('error');
+    res.render('user/signup', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
 });
 
 router.post('/signup', passport.authenticate('local.signup', {
-  successRedirect: '/user/profile',
-  failureRedirect: '/user/signup',
-  failureFlash: true
-}));
+        successRedirect: '/user/profile',
+        failureRedirect: '/user/signup',
+        failureFlash: true
+    }));
 
 router.get('/signin', function (req, res, next) {
-  var messages = req.flash('error');
-  res.render('user/signin', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
+    var messages = req.flash('error');
+    res.render('user/signin', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
 });
 
 router.post('/signin', passport.authenticate('local.signin', {
-  successRedirect: '/user/profile',
-  failureRedirect: '/user/signin',
-  failureFlash: true
+    successRedirect: '/user/profile',
+    failureRedirect: '/user/signin',
+    failureFlash: true
 }));
 
 module.exports = router;
 
 function requiredLogin(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect('/');
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/');
 }
 
 function notRequiredLogin(req, res, next) {
-  if (!req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect('/');
+    if (!req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/');
 }
