f7af4b8c5377b99ab250327cd2ba819970233ab4
vaggelisdel <delliosvaggelis@gmail.com>
2019-11-14 22:38:26 +0200

    user orders history

--- models/cart.js
+++ models/cart.js
@@ -1,21 +1,19 @@
 module.exports = function Cart(oldCart) {
     this.items = oldCart.items || {};
     this.totalQty = oldCart.totalQty || 0;
-    this.totalPrice = parseFloat(oldCart.totalPrice).toFixed(2) || 0;
-
-    if (isNaN(this.totalPrice)){
-        this.totalPrice = 0;
-    }
+    this.totalPrice = oldCart.totalPrice || 0;
 
     this.add = function (item, id) {
         var storedItem = this.items[id];
+        var singlePrice = 0;
         if (!storedItem) {
             storedItem = this.items[id] = {item: item, qty: 0, price: 0};
         }
         storedItem.qty++;
         storedItem.price = storedItem.item.price * storedItem.qty;
         this.totalQty++;
-        this.totalPrice += storedItem.item.price;
+        singlePrice += storedItem.item.price;
+        this.totalPrice = parseFloat(parseFloat(this.totalPrice) + parseFloat(singlePrice)).toFixed(2);
     };
 
     this.generateArray = function () {
@@ -24,3 +22,27 @@ module.exports = function Cart(oldCart) {
         return arr;
     }
 };
+
+// module.exports = function Cart(oldCart) {
+//     this.items = oldCart.items || {};
+//     this.totalQty = oldCart.totalQty || 0;
+//     this.finalPrice = oldCart.finalPrice || 0;
+//     this.totalPrice = parseFloat(this.finalPrice).toFixed(2);
+//
+//     this.add = function (item, id) {
+//         var storedItem = this.items[id];
+//         if (!storedItem) {
+//             storedItem = this.items[id] = {item: item, qty: 0, price: 0};
+//         }
+//         storedItem.qty++;
+//         storedItem.price = storedItem.item.price * storedItem.qty;
+//         this.totalQty++;
+//         this.finalPrice += storedItem.item.price;
+//     };
+//
+//     this.generateArray = function () {
+//         var arr = [];
+//         for (var id in this.items) arr.push(this.items[id]);
+//         return arr;
+//     }
+// };
